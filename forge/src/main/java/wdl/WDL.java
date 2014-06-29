7360c39e9e0d7746e37f936745a8aeb63a2ddd73
Florian Sonnenberger <nairolf@online.de>
2014-06-29 20:13:23 +0200

    Added README for Forge setup, updated .gitignore, fixed wrong file name

--- forge/src/main/java/wdl/WDL.java
+++ forge/src/main/java/wdl/WDL.java
@@ -1,12 +1,12 @@
-package wdl.test;
+package wdl;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = WDL.MODID, version = WDL.VERSION)
+public class WDL
 {
     public static final String MODID = "WorldDownloader";
     public static final String VERSION = "1.7.10";
