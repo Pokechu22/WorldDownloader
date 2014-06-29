c07dd71d3570f3eeae0550e4d6b69584978846f3
Florian Sonnenberger <nairolf@online.de>
2014-06-29 18:22:52 +0200

    Initial groundwork for Forge: gitignore, build file, example code, mod info

---
+++ forge/src/main/java/wdl/test.java
@@ -0,0 +1,20 @@
+package wdl.test;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "WorldDownloader";
+    public static final String VERSION = "1.7.10";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
