9631cb8b95e42857068ecc489e6a6b11c992adc6
Florian Sonnenberger <nairolf@online.de>
2014-06-30 18:15:06 +0200

    Test chunk events

--- forge/src/main/java/wdl/WDL.java
+++ forge/src/main/java/wdl/WDL.java
@@ -1,20 +1,65 @@
 package wdl;
 
-import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.ChunkEvent;
+import net.minecraftforge.event.world.WorldEvent;
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = WDL.MODID, version = WDL.VERSION)
+@Mod( modid = WDL.MODID, version = WDL.VERSION )
 public class WDL
 {
     public static final String MODID = "WorldDownloader";
     public static final String VERSION = "1.7.10";
     
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    public void init( FMLInitializationEvent event )
     {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    	MinecraftForge.EVENT_BUS.register( this );
+    }
+    
+	
+    @SubscribeEvent
+    public void onChunkLoad( ChunkEvent.Load event )
+    {
+    	if( event.world.isRemote )
+    	{
+    		System.out.println("####### Loaded chunk: " + event.getChunk().xPosition + " " + event.getChunk().zPosition );
+    	}
+    }
+    
+    @SubscribeEvent
+    public void onChunkUnload( ChunkEvent.Unload event )
+    {
+    	if( event.world.isRemote )
+    	{
+    		System.out.println("####### Unloaded chunk: " + event.getChunk().xPosition + " " + event.getChunk().zPosition );
+    	}
+    }
+    
+    @SubscribeEvent
+    public void onWorldLoad( WorldEvent.Load event )
+    {
+    	if( event.world.isRemote )
+    	{
+    		System.out.println("####### Loaded world: " + event.world.toString() );
+    	}
+    	else
+    	{
+    		FMLLog.getLogger().error("World Downloader is a client side mod! Please remove it from the server.");
+    	}
+    }
+    
+    @SubscribeEvent
+    public void onWorldUnload( WorldEvent.Unload event )
+    {
+    	if( event.world.isRemote )
+    	{
+    		System.err.println("####### Unloaded world: " + event.world.toString() );
+    	}
     }
 }
