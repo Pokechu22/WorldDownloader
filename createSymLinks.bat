dc6d01b31a7cf41ccd463019b3289afa34d0f4e4
Florian Sonnenberger <nairolf@online.de>
2014-07-15 01:32:04 +0200

    Add script that creates a symbolic link to the shared code

---
+++ createSymLinks.bat
@@ -0,0 +1,10 @@
+REM This script creates symbolic links to the "src" folder (that contains WDL's main classes)
+REM  in every subfolder that needs these shared files.
+REM This is useful to synchronise our shared code base across all variants of the mod.
+REM  They create a writable "view" into into another part of the filesystem.
+REM Symbolic links are transparent to applications and behave like regular files or folders
+REM  with the exception that deleting a symlink does not delete the linked resources.
+
+REM Run this script after you've cloned the repo and anytime this file was changed in a new commit.
+
+mklink /j "mcp\src\minecraft\wdl" "src\wdl"
---
+++ createSymLinks.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# This script creates symbolic links to the "src" folder (that contains WDL's main classes)
+#  in every subfolder that needs these shared files.
+# This is useful to synchronise our shared code base across all variants of the mod.
+#  They create a writable "view" into into another part of the filesystem.
+# Symbolic links are transparent to applications and behave like regular files or folders
+#  with the exception that deleting a symlink does not delete the linked resources.
+
+# Run this script after you've cloned the repo and anytime this file was changed in a new commit.
+
+ln -sr "src/wdl" "mcp/src/minecraft/wdl"
