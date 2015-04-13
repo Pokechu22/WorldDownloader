a00f77594761d9107e64bfc45e85509dcb6a2083
Pokechu22 <Pokechu022@gmail.com>
2015-04-13 15:49:52 -0700

    Changed the client brand to something different.

---
+++ mcp/src/minecraft/net/minecraft/client/ClientBrandRetriever.java
@@ -0,0 +1,11 @@
+package net.minecraft.client;
+
+public class ClientBrandRetriever
+{
+    private static final String __OBFID = "CL_00001460";
+
+    public static String getClientModName()
+    {
+        return "worlddownloader-vanilla";
+    }
+}
