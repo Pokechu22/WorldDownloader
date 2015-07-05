80a01914f3e369f9df39bd72fbd9177057bbb17b
Pokechu22 <Pokechu022@gmail.com>
2015-07-05 14:07:10 -0700

    Set version to 1.8c-beta1.
    This is going to be a test release so that @gourdo1 can verify if #7 and
    #8 are still present.

--- mcp/src/minecraft/net/minecraft/client/ClientBrandRetriever.java
+++ mcp/src/minecraft/net/minecraft/client/ClientBrandRetriever.java
@@ -1,9 +1,10 @@
+
 package net.minecraft.client;
 
 public class ClientBrandRetriever {
 	private static final String __OBFID = "CL_00001460";
 
 	public static String getClientModName() {
-		return "worlddownloader1.8c-coremod";
+		return "worlddownloader1.8c-beta1-coremod";
 	}
 }
