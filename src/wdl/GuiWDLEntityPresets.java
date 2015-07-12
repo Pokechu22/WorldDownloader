8746d6627dc7492627d596d97b5dd9b3dfa10797
Pokechu22 <Pokechu022@gmail.com>
2015-07-11 22:51:24 -0700

    Added entity enable/disable buttons (not implemented)

--- src/wdl/GuiWDLEntityPresets.java
+++ src/wdl/GuiWDLEntityPresets.java
@@ -17,6 +17,12 @@ public class GuiWDLEntityPresets extends GuiScreen implements GuiYesNoCallback {
 	private GuiButton vanillaButton;
 	private GuiButton spigotButton;
 	private GuiButton serverButton;
+	private GuiButton allOtherButton;
+	private GuiButton noOtherButton;
+	private GuiButton allPassiveButton;
+	private GuiButton noPassiveButton;
+	private GuiButton allHostileButton;
+	private GuiButton noHostileButton;
 	private GuiButton okButton;
 	
 	public GuiWDLEntityPresets(GuiScreen parent) {
@@ -29,20 +35,43 @@ public class GuiWDLEntityPresets extends GuiScreen implements GuiYesNoCallback {
 		
 		this.vanillaButton = new GuiButton(0, this.width / 2 - 230, y, 150, 20,
 				"Vanilla minecraft ranges");
-		this.buttonList.add(vanillaButton);
-		
 		this.spigotButton = new GuiButton(1, this.width / 2 - 75, y, 150, 20,
 				"Default spigot ranges");
-		this.buttonList.add(spigotButton);
-		
 		this.serverButton = new GuiButton(2, this.width / 2 + 80, y, 150, 20,
 				"Server-configured ranges");
+		
 		serverButton.enabled = WDLPluginChannels.hasServerEntityRange();
+		
+		this.buttonList.add(vanillaButton);
+		this.buttonList.add(spigotButton);
 		this.buttonList.add(serverButton);
 		
 		y += 28 + 2 + mc.fontRendererObj.FONT_HEIGHT;
 		
-		y += 28;
+		this.allOtherButton = new GuiButton(3, this.width / 2 - 230, y, 150, 20,
+				"Enable all other entities");
+		this.allPassiveButton = new GuiButton(4, this.width / 2 - 75, y, 150, 20,
+				"Enable all passive entities");
+		this.allHostileButton = new GuiButton(5, this.width / 2 + 80, y, 150, 20,
+				"Enable all hostile entities");
+		
+		y += 22;
+		
+		this.noOtherButton = new GuiButton(6, this.width / 2 - 230, y, 150, 20,
+				"Disable all other entities");
+		this.noPassiveButton = new GuiButton(7, this.width / 2 - 75, y, 150, 20,
+				"Disable all passive entities");
+		this.noHostileButton = new GuiButton(8, this.width / 2 + 80, y, 150, 20,
+				"Disable all hostile entities");
+		
+		this.buttonList.add(allOtherButton);
+		this.buttonList.add(allPassiveButton);
+		this.buttonList.add(allHostileButton);
+		this.buttonList.add(noOtherButton);
+		this.buttonList.add(noPassiveButton);
+		this.buttonList.add(noHostileButton);
+		
+		y += 28 + 2 + mc.fontRendererObj.FONT_HEIGHT;
 		
 		this.okButton = new GuiButton(100, this.width / 2 - 100, y, "OK");
 		this.buttonList.add(okButton);
@@ -54,9 +83,9 @@ public class GuiWDLEntityPresets extends GuiScreen implements GuiYesNoCallback {
 			return;
 		}
 		
-		if (button.id < 100) {
+		if (button.id < 3) {
 			mc.displayGuiScreen(new GuiYesNo(this,
-					"Are you sure you want to change your entity ranges?",
+					"Are you sure you want to reset your entity ranges?",
 					"(Setting to " + button.displayString + ")", button.id));
 		}
 		if (button.id == 100) {
@@ -72,6 +101,8 @@ public class GuiWDLEntityPresets extends GuiScreen implements GuiYesNoCallback {
 				this.width / 2, this.height / 4 - 40, 0xFFFFFF);
 		this.drawCenteredString(this.fontRendererObj, "Ranges", this.width / 2,
 				this.height / 4 - fontRendererObj.FONT_HEIGHT - 2, 0xFFFFFF);
+		this.drawCenteredString(this.fontRendererObj, "Enable / Disable",
+				this.width / 2, this.height / 4 + 28, 0xFFFFFF);
 		
 		int infoX = this.width / 2 - 150;
 		int infoY = 2 * this.height / 3;
@@ -118,6 +149,7 @@ public class GuiWDLEntityPresets extends GuiScreen implements GuiYesNoCallback {
 						"cannot be used.", infoX + 5, y, 0xFFFFFF);
 			}
 		}
+		//TODO: Entity button mouse overs.
 		
 		super.drawScreen(mouseX, mouseY, partialTicks);
 	}
