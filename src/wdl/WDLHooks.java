f74de15627d70ab97dfa54281dd375cd2d55db18
Pokechu22 <Pokechu022@gmail.com>
2015-07-16 22:23:19 -0700

    Fixed profiler issues due to the previous change.
    If downloading wasn't occuring, it wouldn't end the section.

--- src/wdl/WDLHooks.java
+++ src/wdl/WDLHooks.java
@@ -57,10 +57,8 @@ public class WDLHooks {
 				
 				profiler.endSection();
 			} else {
-				if (!WDL.downloading) { return; }
-				
 				profiler.startSection("inventoryCheck");
-				if (WDL.thePlayer != null) {
+				if (WDL.downloading && WDL.thePlayer != null) {
 					if (WDL.thePlayer.openContainer != WDL.windowContainer) {
 						if (WDL.thePlayer.openContainer == WDL.thePlayer.inventoryContainer) {
 							profiler.startSection("onItemGuiClosed");
