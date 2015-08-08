fc0aa83e99e14d2cf87772d789a81dc93bb079db
Pokechu22 <Pokechu022@gmail.com>
2015-08-08 13:56:29 -0700

    GuiWDLDebug now scrolls.

--- src/wdl/gui/GuiWDLDebug.java
+++ src/wdl/gui/GuiWDLDebug.java
@@ -1,14 +1,93 @@
 package wdl.gui;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import wdl.WDL;
 import wdl.WDLDebugMessageCause;
+import wdl.WDLPluginChannels;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiListExtended;
 import net.minecraft.client.gui.GuiScreen;
 
 public class GuiWDLDebug extends GuiScreen {
+	private class GuiDebugList extends GuiListExtended {
+		public GuiDebugList() {
+			super(GuiWDLDebug.this.mc, GuiWDLDebug.this.width,
+					GuiWDLDebug.this.height, 39,
+					GuiWDLDebug.this.height - 32, 20);
+		}
+
+		private class DebugEntry implements IGuiListEntry {
+			private final GuiButton button;
+			private final WDLDebugMessageCause type;
+			
+			public DebugEntry(WDLDebugMessageCause type) {
+				this.type = type;
+				this.button = new GuiButton(0, 0, 0, type.toString());
+			}
+			
+			@Override
+			public void setSelected(int p_178011_1_, int p_178011_2_,
+					int p_178011_3_) {
+				
+			}
+
+			@Override
+			public void drawEntry(int slotIndex, int x, int y, int listWidth,
+					int slotHeight, int mouseX, int mouseY, boolean isSelected) {
+				button.xPosition = GuiWDLDebug.this.width / 2 - 100;
+				button.yPosition = y;
+				
+				button.displayString = type.toString();
+				button.enabled = WDLDebugMessageCause.globalDebugEnabled;
+				
+				button.drawButton(mc, mouseX, mouseY);
+			}
+
+			@Override
+			public boolean mousePressed(int slotIndex, int x, int y,
+					int mouseEvent, int relativeX, int relativeY) {
+				if (button.mousePressed(mc, x, y)) {
+					type.toggleEnabled();
+					
+					button.playPressSound(mc.getSoundHandler());
+					
+					return true;
+				}
+				
+				return false;
+			}
+
+			@Override
+			public void mouseReleased(int slotIndex, int x, int y,
+					int mouseEvent, int relativeX, int relativeY) {
+				
+			}
+		}
+		
+		private List<IGuiListEntry> entries = new ArrayList<IGuiListEntry>() {{
+			for (WDLDebugMessageCause cause : WDLDebugMessageCause.values()) {
+				add(new DebugEntry(cause));
+			}
+		}};
+		
+		@Override
+		public IGuiListEntry getListEntry(int index) {
+			return entries.get(index);
+		}
+
+		@Override
+		protected int getSize() {
+			return entries.size();
+		}
+		
+	}
+	
 	private GuiScreen parent;
+	private GuiDebugList list;
 	
 	public GuiWDLDebug(GuiScreen parent) {
 		this.parent = parent;
@@ -19,28 +98,14 @@ public class GuiWDLDebug extends GuiScreen {
 	@Override
 	public void initGui() {
 		int x = (this.width / 2) - 100;
-		int y = (this.height / 4) - 15;
 		
-		masterDebugSwitch = new GuiButton(100, x, y, "Master debug switch: " + 
+		masterDebugSwitch = new GuiButton(100, x, 18, "Master debug switch: " + 
 				(WDLDebugMessageCause.globalDebugEnabled ? "On" : "Off"));
 		this.buttonList.add(masterDebugSwitch);
 		
-		WDLDebugMessageCause[] causes = WDLDebugMessageCause.values();
+		this.list = new GuiDebugList();
 		
-		y += 28;
-		
-		for (int i = 0; i < causes.length; i++) {
-			GuiButton causeBtn = new GuiButton(i, x, y, causes[i].toString());
-			causeBtn.enabled = WDLDebugMessageCause.globalDebugEnabled;
-			
-			this.buttonList.add(causeBtn);
-			
-			y += 22;
-		}
-		
-		y += 6;
-		
-		this.buttonList.add(new GuiButton(101, x, y, "Done"));
+		this.buttonList.add(new GuiButton(101, x, this.height - 29, "Done"));
 	}
 	
 	@Override
@@ -55,22 +120,8 @@ public class GuiWDLDebug extends GuiScreen {
 			
 			button.displayString = "Master debug switch: " + 
 					(WDLDebugMessageCause.globalDebugEnabled ? "On" : "Off");
-			
-			for (Object obj : buttonList) {
-				GuiButton btn = (GuiButton)obj;
-				if (btn.id < 100) {
-					btn.enabled = WDLDebugMessageCause.globalDebugEnabled;
-				}
-			}
 		} else if (button.id == 101) {
 			this.mc.displayGuiScreen(this.parent);
-		} else {
-			WDLDebugMessageCause cause = 
-					WDLDebugMessageCause.values()[button.id];
-			
-			cause.toggleEnabled();
-			
-			button.displayString = cause.toString();
 		}
 	}
 	
@@ -85,11 +136,39 @@ public class GuiWDLDebug extends GuiScreen {
 		WDL.saveProps();
 	}
 	
+	/**
+	 * Handles mouse input.
+	 */
+	@Override
+	public void handleMouseInput() throws IOException {
+		super.handleMouseInput();
+		this.list.func_178039_p();
+	}
+	
+	@Override
+	protected void mouseClicked(int mouseX, int mouseY, int mouseButton)
+			throws IOException {
+		if (list.func_148179_a(mouseX, mouseY, mouseButton)) {
+			return;
+		}
+		super.mouseClicked(mouseX, mouseY, mouseButton);
+	}
+	
+	@Override
+	protected void mouseReleased(int mouseX, int mouseY, int state) {
+		if (list.func_148181_b(mouseX, mouseY, state)) {
+			return;
+		}
+		super.mouseReleased(mouseX, mouseY, state);
+	}
+	
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 		this.drawDefaultBackground();
+		this.list.drawScreen(mouseX, mouseY, partialTicks);
+		
 		this.drawCenteredString(this.fontRendererObj, "Debug options",
-				this.width / 2, this.height / 4 - 40, 0xFFFFFF);
+				this.width / 2, 8, 0xFFFFFF);
 		
 		super.drawScreen(mouseX, mouseY, partialTicks);
 	}
