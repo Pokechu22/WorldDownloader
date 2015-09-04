c5a0e09ec61b6f100378c36382ffad7fb2915b30
Pokechu22 <Pokechu022@gmail.com>
2015-09-03 21:45:19 -0700

    Use an actual list control, and update some of the text.

--- src/wdl/gui/GuiWDLPermissions.java
+++ src/wdl/gui/GuiWDLPermissions.java
@@ -6,13 +6,42 @@ import wdl.WDL;
 import wdl.WDLPluginChannels;
 import wdl.WorldBackup.WorldBackupType;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiListExtended;
 import net.minecraft.client.gui.GuiScreen;
 
 /**
  * GUI that shows the current permissions for the user.
  */
 public class GuiWDLPermissions extends GuiScreen {
+	/**
+	 * Margins for the top and the bottom of the list.
+	 */
+	private static final int TOP_MARGIN = 23, BOTTOM_MARGIN = 32;
+	
+	/**
+	 * List of permissions.
+	 */
+	private class PermissionsList extends GuiListExtended {
+		public PermissionsList() {
+			super(GuiWDLPermissions.this.mc, GuiWDLPermissions.this.width,
+					GuiWDLPermissions.this.height, TOP_MARGIN,
+					GuiWDLPermissions.this.height - BOTTOM_MARGIN, 20);
+		}
+		
+		@Override
+		public IGuiListEntry getListEntry(int index) {
+			return null;
+		}
+		
+		@Override
+		protected int getSize() {
+			return 0;
+		}
+	}
+	
 	private final GuiScreen parent;
+	
+	private PermissionsList list;
 	
 	public GuiWDLPermissions(GuiScreen parent) {
 		this.parent = parent;
@@ -22,6 +51,34 @@ public class GuiWDLPermissions extends GuiScreen {
 	public void initGui() {
 		this.buttonList.add(new GuiButton(100, width / 2 - 100, height - 29,
 				"Done"));
+		this.list = new PermissionsList();
+	}
+	
+	/**
+	 * Called when the mouse is clicked.
+	 */
+	@Override
+	protected void mouseClicked(int mouseX, int mouseY, int mouseButton)
+	throws IOException {
+		list.func_148179_a(mouseX, mouseY, mouseButton);
+		super.mouseClicked(mouseX, mouseY, mouseButton);
+	}
+	
+	/**
+	 * Handles mouse input.
+	 */
+	@Override
+	public void handleMouseInput() throws IOException {
+		super.handleMouseInput();
+		this.list.func_178039_p();
+	}
+	
+	@Override
+	protected void mouseReleased(int mouseX, int mouseY, int state) {
+		if (list.func_148181_b(mouseX, mouseY, state)) {
+			return;
+		}
+		super.mouseReleased(mouseX, mouseY, state);
 	}
 	
 	@Override
@@ -33,20 +90,15 @@ public class GuiWDLPermissions extends GuiScreen {
 	
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-		Utils.drawBorder(23, 32, 0, 0, height, width);
+		this.list.drawScreen(mouseX, mouseY, partialTicks);
 		
 		this.drawCenteredString(this.fontRendererObj, "Permission info",
 				this.width / 2, 8, 0xFFFFFF);
 		
 		if (!WDLPluginChannels.hasPermissions()) {
 			this.drawCenteredString(this.fontRendererObj,
-					"No permissions loaded.", this.width / 2,
-					(this.height - 32 - 23) / 2 + 23
-							- fontRendererObj.FONT_HEIGHT / 2, 0xFFFFFF);
-		} else {
-			this.drawCenteredString(this.fontRendererObj,
-					"Some permissions loaded.", this.width / 2,
-					(this.height - 32 - 23) / 2 + 23
+					"No permissions sent; defaulting to everything enabled.",
+					this.width / 2, (this.height - 32 - 23) / 2 + 23
 							- fontRendererObj.FONT_HEIGHT / 2, 0xFFFFFF);
 		}
 		
