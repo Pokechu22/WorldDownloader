9ebf3eb8d6bd16f3c5381e6d2f970b0d16af4782
Pokechu22 <Pokechu022@gmail.com>
2015-10-30 16:37:18 -0700

    Add I18n for GuiWDLGenerator.

--- src/wdl/gui/GuiWDLGenerator.java
+++ src/wdl/gui/GuiWDLGenerator.java
@@ -6,16 +6,19 @@ import wdl.WDL;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.client.resources.I18n;
 
 public class GuiWDLGenerator extends GuiScreen {
-	private String title = "";
+	private String title;
 	private GuiScreen parent;
 	private GuiTextField seedField;
 	private GuiButton generatorBtn;
 	private GuiButton generateStructuresBtn;
 
-	public GuiWDLGenerator(GuiScreen var1) {
-		this.parent = var1;
+	private String seedText;
+	
+	public GuiWDLGenerator(GuiScreen parent) {
+		this.parent = parent;
 	}
 
 	/**
@@ -23,28 +26,27 @@ public class GuiWDLGenerator extends GuiScreen {
 	 */
 	@Override
 	public void initGui() {
-		this.buttonList.clear();
-		this.title = "World Generator Options for "
-				+ WDL.baseFolderName.replace('@', ':');
-		int var1 = this.width / 2;
-		int var2 = this.height / 4;
-		int var3 = var2 - 15;
-		this.seedField = new GuiTextField(40, this.fontRendererObj,
-				this.width / 2 - 70, var3, 168, 18);
-		this.seedField.setText("ERROR");
-		this.updateSeed(false);
-		var3 += 22;
-		this.generatorBtn = new GuiButton(1, var1 - 100, var3,
-				"World Generator: ERROR");
-		this.buttonList.add(this.generatorBtn);
-		this.updateGenerator(false);
-		var3 += 22;
-		this.generateStructuresBtn = new GuiButton(2, var1 - 100, var3,
-				"Generate Structures: ERROR");
-		this.buttonList.add(this.generateStructuresBtn);
-		this.updateGenerateStructures(false);
+		this.seedText = I18n.format("wdl.gui.generator.seed");
+		int seedWidth = fontRendererObj.getStringWidth(seedText + " ");
 		
-		this.buttonList.add(new GuiButton(100, var1 - 100, height - 29, "Done"));
+		this.buttonList.clear();
+		this.title = I18n.format("wdl.gui.generator.title",
+				 WDL.baseFolderName.replace('@', ':'));
+		int y = this.height / 4 - 15;
+		this.seedField = new GuiTextField(40, this.fontRendererObj,
+				this.width / 2 - (100 - seedWidth), y, 200 - seedWidth, 18);
+		this.seedField.setText(WDL.worldProps.getProperty("RandomSeed"));
+		y += 22;
+		this.generatorBtn = new GuiButton(1, this.width / 2 - 100, y,
+				getGeneratorText());
+		this.buttonList.add(this.generatorBtn);
+		y += 22;
+		this.generateStructuresBtn = new GuiButton(2, this.width / 2 - 100, y,
+				getGenerateStructuresText());
+		this.buttonList.add(this.generateStructuresBtn);
+		
+		this.buttonList.add(new GuiButton(100, this.width / 2 - 100, height - 29, 
+				I18n.format("gui.done")));
 	}
 
 	/**
@@ -52,13 +54,13 @@ public class GuiWDLGenerator extends GuiScreen {
 	 * ActionListener.actionPerformed(ActionEvent e).
 	 */
 	@Override
-	protected void actionPerformed(GuiButton var1) {
-		if (var1.enabled) {
-			if (var1.id == 1) {
-				this.updateGenerator(true);
-			} else if (var1.id == 2) {
-				this.updateGenerateStructures(true);
-			} else if (var1.id == 100) {
+	protected void actionPerformed(GuiButton button) {
+		if (button.enabled) {
+			if (button.id == 1) {
+				this.cycleGenerator();
+			} else if (button.id == 2) {
+				this.cycleGenerateStructures();
+			} else if (button.id == 100) {
 				this.mc.displayGuiScreen(this.parent);
 			}
 		}
@@ -66,7 +68,8 @@ public class GuiWDLGenerator extends GuiScreen {
 	
 	@Override
 	public void onGuiClosed() {
-		this.updateSeed(true);
+		WDL.worldProps.setProperty("RandomSeed", this.seedField.getText());
+		
 		WDL.saveProps();
 	}
 
@@ -108,7 +111,8 @@ public class GuiWDLGenerator extends GuiScreen {
 		
 		this.drawCenteredString(this.fontRendererObj, this.title,
 				this.width / 2, 8, 0xFFFFFF);
-		this.drawString(this.fontRendererObj, "Seed:", this.width / 2 - 99,
+		
+		this.drawString(this.fontRendererObj, seedText, this.width / 2 - 100,
 				this.height / 4 - 10, 0xFFFFFF);
 		this.seedField.drawTextBox();
 		super.drawScreen(mouseX, mouseY, partialTicks);
@@ -116,60 +120,44 @@ public class GuiWDLGenerator extends GuiScreen {
 		String tooltip = null;
 		
 		if (Utils.isMouseOverTextBox(mouseX, mouseY, seedField)) {
-			tooltip = "Set the seed used for generating new chunks in " +
-					"the downloaded world.";
+			tooltip = I18n.format("wdl.gui.generator.seed.description");
 		} else if (generatorBtn.isMouseOver()) {
-			tooltip = "The world generator used when generating new chunks.";
+			tooltip = I18n.format("wdl.gui.generator.generator.description");
 		} else if (generateStructuresBtn.isMouseOver()) {
-			tooltip = "Whether structures (villages, strongholds, ect) " +
-					"should be generated in new chunks.";
+			tooltip = I18n.format("wdl.gui.generator.generateStructures.description");
 		}
 		Utils.drawGuiInfoBox(tooltip, width, height);
 	}
 
-	private void updateGenerator(boolean var1) {
-		String var2 = WDL.worldProps.getProperty("GeneratorName");
-
-		if (var2.equals("default")) {
-			if (var1) {
-				WDL.worldProps.setProperty("GeneratorName", "flat");
-				WDL.worldProps.setProperty("GeneratorVersion", "0");
-				this.updateGenerator(false);
-			} else {
-				this.generatorBtn.displayString = "World Generator: Default";
-			}
-		} else if (var1) {
+	private void cycleGenerator() {
+		if (WDL.worldProps.getProperty("GeneratorName").equals("default")) {
+			WDL.worldProps.setProperty("GeneratorName", "flat");
+			WDL.worldProps.setProperty("GeneratorVersion", "0");
+		} else {
 			WDL.worldProps.setProperty("GeneratorName", "default");
 			WDL.worldProps.setProperty("GeneratorVersion", "1");
-			this.updateGenerator(false);
-		} else {
-			this.generatorBtn.displayString = "World Generator: Flat";
 		}
+		
+		this.generatorBtn.displayString = getGeneratorText();
 	}
 
-	private void updateGenerateStructures(boolean var1) {
-		String var2 = WDL.worldProps.getProperty("MapFeatures");
-
-		if (var2.equals("true")) {
-			if (var1) {
-				WDL.worldProps.setProperty("MapFeatures", "false");
-				this.updateGenerateStructures(false);
-			} else {
-				this.generateStructuresBtn.displayString = "Generate Structures: ON";
-			}
-		} else if (var1) {
+	private void cycleGenerateStructures() {
+		if (WDL.worldProps.getProperty("MapFeatures").equals("true")) {
+			WDL.worldProps.setProperty("MapFeatures", "false");
+		} else {
 			WDL.worldProps.setProperty("MapFeatures", "true");
-			this.updateGenerateStructures(false);
-		} else {
-			this.generateStructuresBtn.displayString = "Generate Structures: OFF";
 		}
+		
+		this.generateStructuresBtn.displayString = getGenerateStructuresText();
 	}
-
-	private void updateSeed(boolean var1) {
-		if (var1) {
-			WDL.worldProps.setProperty("RandomSeed", this.seedField.getText());
-		} else {
-			this.seedField.setText(WDL.worldProps.getProperty("RandomSeed"));
-		}
+	
+	private String getGeneratorText() {
+		return I18n.format("wdl.gui.generator.generator." + 
+				WDL.worldProps.getProperty("GeneratorName"));
+	}
+	
+	private String getGenerateStructuresText() {
+		return I18n.format("wdl.gui.generator.generateStructures." +
+				WDL.worldProps.getProperty("MapFeatures"));
 	}
 }
