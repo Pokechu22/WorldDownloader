e1404f8daa72af5f1425518ed215569f257a56cd
Pokechu22 <Pokechu022@gmail.com>
2015-12-05 14:57:11 -0800

    Use a TextList for GuiWDLChunkOverrides.

--- src/wdl/gui/GuiWDLChunkOverrides.java
+++ src/wdl/gui/GuiWDLChunkOverrides.java
@@ -1,14 +1,10 @@
 package wdl.gui;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import wdl.WDLPluginChannels;
 import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiListExtended;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.GuiListExtended.IGuiListEntry;
+import wdl.WDLPluginChannels;
 
 /**
  * A GUI that Lists... well, will list, the current chunk overrides.  Currently
@@ -19,81 +15,7 @@ import net.minecraft.client.gui.GuiListExtended.IGuiListEntry;
 public class GuiWDLChunkOverrides extends GuiScreen {
 	private static final int TOP_MARGIN = 61, BOTTOM_MARGIN = 32;
 	
-	private class OverridesList extends GuiListExtended {
-		public OverridesList(String text) {
-			super(GuiWDLChunkOverrides.this.mc, GuiWDLChunkOverrides.this.width,
-					GuiWDLChunkOverrides.this.height, TOP_MARGIN,
-					GuiWDLChunkOverrides.this.height - BOTTOM_MARGIN, 
-					fontRendererObj.FONT_HEIGHT + 1);
-			
-			this.entries = new ArrayList<TextEntry>();
-			
-			List<String> lines = Utils.wordWrap(text, getListWidth());
-			
-			for (String s : lines) {
-				entries.add(new TextEntry(s));
-			}
-		}
-		
-		private final List<TextEntry> entries;
-		
-		@Override
-		public IGuiListEntry getListEntry(int index) {
-			return entries.get(index);
-		}
-		
-		@Override
-		protected int getSize() {
-			return entries.size();
-		}
-		
-		@Override
-		protected int getScrollBarX() {
-			return GuiWDLChunkOverrides.this.width - 10;
-		}
-		
-		@Override
-		public int getListWidth() {
-			return GuiWDLChunkOverrides.this.width - 18;
-		}
-	}
-	
-	private class TextEntry implements IGuiListEntry {
-		private final String line;
-		
-		/**
-		 * Creates a TextEntry.
-		 */
-		public TextEntry(String line) {
-			this.line = line;
-		}
-		
-		@Override
-		public void drawEntry(int slotIndex, int x, int y, int listWidth,
-				int slotHeight, int mouseX, int mouseY, boolean isSelected) {
-			fontRendererObj.drawString(line, x, y + 1, 0xFFFFFF);
-		}
-
-		@Override
-		public boolean mousePressed(int slotIndex, int x, int y,
-				int mouseEvent, int relativeX, int relativeY) {
-			return false;
-		}
-
-		@Override
-		public void mouseReleased(int slotIndex, int x, int y,
-				int mouseEvent, int relativeX, int relativeY) {
-			
-		}
-
-		@Override
-		public void setSelected(int slotIndex, int p_178011_2_,
-				int p_178011_3_) {
-			
-		}
-	}
-	
-	private OverridesList list;
+	private TextList list;
 	/**
 	 * Parent GUI screen; displayed when this GUI is closed.
 	 */
@@ -106,13 +28,15 @@ public class GuiWDLChunkOverrides extends GuiScreen {
 	@SuppressWarnings("unchecked")
 	@Override
 	public void initGui() {
-		String text = "§c§lThis is a work in progress.§r\nYou can download " +
-				"in overriden chunks even if you are not allowed to download " +
-				"elsewhere on the server.\nHere is a list of the current " +
-				"chunk overrides; in the future, a map will appear here.  " +
-				"Maybe also there will be a minimap mod integration.\n\n";
-		text += WDLPluginChannels.getChunkOverrides().toString();
-		this.list = new OverridesList(text);
+		this.list = new TextList(mc, width, height, TOP_MARGIN, BOTTOM_MARGIN);
+		list.addLine("§c§lThis is a work in progress.");
+		list.addLine("You can download in overriden chunks even if you are " +
+				"not allowed to download elsewhere on the server.");
+		list.addLine("Here is a list of the current chunk overrides; in the " +
+				"future, a map will appear here.");
+		list.addLine("Maybe also there will be a minimap mod integration.");
+		list.addBlankLine();
+		list.addLine(WDLPluginChannels.getChunkOverrides().toString());
 		
 		this.buttonList.add(new GuiButton(100, width / 2 - 100, height - 29,
 				"Done"));
