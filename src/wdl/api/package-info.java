85a5b52db27caa323c48da050e0b1d3e22c56382
Pokechu22 <Pokechu022@gmail.com>
2015-12-25 21:30:39 -0800

    Update API description.

--- src/wdl/api/package-info.java
+++ src/wdl/api/package-info.java
@@ -1,4 +1,10 @@
 /**
- * Contains an API for mods to handle new blocks.
+ * <h1>World Downloader API</h1>
+ * 
+ * You can create extensions for this mod using the API.  Simply implement
+ * some of the WDL interfaces, and then call {@link WDLApi#addWDLMod(IWDLMod)}.
+ * 
+ * Note that you do need to handle loading the mod yourself - use liteloader
+ * or Minecraft Forge for that.
  */
 package wdl.api;
