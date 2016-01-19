2695743c7a00abf83af2d4f0c900693423eb4442
Pokechu22 <Pokechu022@gmail.com>
2016-01-18 17:44:01 -0800

    Change version to 1.8d to prevent bad hash warnings in dev environment.

--- src/wdl/WDL.java
+++ src/wdl/WDL.java
@@ -87,7 +87,7 @@ public class WDL {
 	/**
 	 * Current version.  This should match the git tag for the current release.
 	 */
-	public static final String VERSION = "1.8d-beta1";
+	public static final String VERSION = "1.8d";
 	/**
 	 * The version of minecraft that this mod is installed on.
 	 */
