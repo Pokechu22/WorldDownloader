3486058d0e03995ac5aae0dfc42869ce5423f5e1
Pokechu22 <Pokechu022@gmail.com>
2016-03-06 13:44:36 -0800

    Mark version as 1.9a-beta2.

--- src/wdl/WDL.java
+++ src/wdl/WDL.java
@@ -90,7 +90,7 @@ public class WDL {
 	/**
 	 * Current version.  This should match the git tag for the current release.
 	 */
-	public static final String VERSION = "1.9a-beta1";
+	public static final String VERSION = "1.9a-beta2";
 	/**
 	 * The version of minecraft that this mod is installed on.
 	 */
