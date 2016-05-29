0f66b992b3649837566b693306a90e8b0273ccff
Pokechu22 <Pokechu022@gmail.com>
2016-05-28 19:16:19 -0700

    Change VERSION to 1.8d again
    This is to prevent hashing warnings in the development environment.
    Eventually, I'll need a better solution for this.

--- src/wdl/WDL.java
+++ src/wdl/WDL.java
@@ -68,7 +68,7 @@ public class WDL {
 	/**
 	 * Current version.  This should match the git tag for the current release.
 	 */
-	public static final String VERSION = "1.8d-beta2";
+	public static final String VERSION = "1.8d";
 	/**
 	 * The version of minecraft that this mod is installed on.
 	 */
