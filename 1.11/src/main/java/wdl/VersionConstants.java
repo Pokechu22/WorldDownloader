d9627d9f9fdf56f4bf93cc66d032478d4d6db9a8
Pokechu22 <Pokechu022@gmail.com>
2017-08-28 14:48:28 -0700

    Set version to 4.0.1.1-SNAPSHOT for development

--- 1.11/src/main/java/wdl/VersionConstants.java
+++ 1.11/src/main/java/wdl/VersionConstants.java
@@ -72,6 +72,6 @@ public class VersionConstants {
 	 */
 	public static String getModVersion() {
 		// TODO: Automatically generate this somehow
-		return "4.0.1.0";
+		return "4.0.1.1-SNAPSHOT";
 	}
 }
