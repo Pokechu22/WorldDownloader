7fdb6fa48d9f5f96c64b5cf2a69db01d73d63b36
Pokechu22 <Pokechu022@gmail.com>
2017-09-19 12:30:37 -0700

    1.12.2 litemod

---
+++ 1.12.2-litemod/src/main/java/com/uyjulian/LiteModWDL/mixin/MixinNetHandlerPlayClient.java
@@ -0,0 +1,45 @@
+package com.uyjulian.LiteModWDL.mixin;
+
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.network.play.INetHandlerPlayClient;
+import net.minecraft.network.play.server.SPacketBlockAction;
+import net.minecraft.network.play.server.SPacketChat;
+import net.minecraft.network.play.server.SPacketCustomPayload;
+import net.minecraft.network.play.server.SPacketMaps;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NetHandlerPlayClient.class)
+public abstract class MixinNetHandlerPlayClient implements INetHandlerPlayClient {
+	@Inject(method="handleChat", at=@At("RETURN"))
+	private void onHandleChat(SPacketChat p_147251_1_, CallbackInfo ci) {
+		//more up here
+		/* WDL >>> */
+		wdl.WDLHooks.onNHPCHandleChat((NetHandlerPlayClient)(Object)this, p_147251_1_);
+		/* <<< WDL */
+	}
+	@Inject(method="handleBlockAction", at=@At("RETURN"))
+	private void onHandleBlockAction(SPacketBlockAction packetIn, CallbackInfo ci) {
+		//more up here
+		/* WDL >>> */
+		wdl.WDLHooks.onNHPCHandleBlockAction((NetHandlerPlayClient)(Object)this, packetIn);
+		/* <<< WDL */
+	}
+	@Inject(method="handleMaps", at=@At("RETURN"))
+	private void onHandleMaps(SPacketMaps packetIn, CallbackInfo ci) {
+		//more up here
+		/* WDL >>> */
+		wdl.WDLHooks.onNHPCHandleMaps((NetHandlerPlayClient)(Object)this, packetIn);
+		/* <<< WDL */
+	}
+	@Inject(method="handleCustomPayload", at=@At("RETURN"))
+	private void onHandleCustomPayload(SPacketCustomPayload packetIn, CallbackInfo ci) {
+		//more up here
+		/* WDL >>> */
+		wdl.WDLHooks.onNHPCHandleCustomPayload((NetHandlerPlayClient)(Object)this, packetIn);
+		/* <<< WDL */
+	}
+}
