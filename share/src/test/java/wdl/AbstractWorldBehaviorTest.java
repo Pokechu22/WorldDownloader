44f52754f28b77adb51c51ff684fff480b4a44d8
Pokechu22 <Pokechu022@gmail.com>
2017-11-12 15:49:17 -0800

    Fancier test, this time actually emulating client/server
    Still not perfect, but it has most of the framework I want.

--- share/src/test/java/wdl/AbstractWorldBehaviorTest.java
+++ share/src/test/java/wdl/AbstractWorldBehaviorTest.java
@@ -14,19 +14,42 @@
  */
 package wdl;
 
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Before;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockChest;
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.player.inventory.ContainerLocalMenu;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Bootstrap;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ContainerBeacon;
+import net.minecraft.inventory.ContainerBrewingStand;
+import net.minecraft.inventory.ContainerChest;
+import net.minecraft.inventory.ContainerDispenser;
+import net.minecraft.inventory.ContainerFurnace;
+import net.minecraft.inventory.ContainerHopper;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryBasic;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.IInteractionObject;
+import net.minecraft.world.World;
 
 /**
  * Base logic shared between all tests that use blocks.
@@ -36,6 +59,14 @@ import net.minecraft.world.IBlockAccess;
  */
 public abstract class AbstractWorldBehaviorTest {
 	private static final Logger LOGGER = LogManager.getLogger();
+	/** Worlds corresponding to what the server and client know */
+	protected World serverWorld, clientWorld;
+	/** A player entity.  Has a valid inventory. */
+	protected EntityPlayer player = mock(EntityPlayer.class);
+	/** A set containing all original TEs. */
+	protected Set<BlockPos> origTEPoses;
+	/** A map of block entities for the user to save into. */
+	protected Map<BlockPos, TileEntity> tileEntities;
 
 	@Before
 	public void initBootstarp() {
@@ -55,16 +86,20 @@ public abstract class AbstractWorldBehaviorTest {
 	 *
 	 * @return A mock world, with default behavior.
 	 */
-	protected IBlockAccess makeMockWorld() {
-		IBlockAccess world = mock(IBlockAccess.class);
+	protected void makeMockWorld() {
+		clientWorld = mock(World.class);
+		serverWorld = mock(World.class);
+		tileEntities = new HashMap<>();
+		origTEPoses = new HashSet<>();
 
-		when(world.getBlockState(any())).thenReturn(Blocks.AIR.getDefaultState());
-		when(world.getTileEntity(any())).thenReturn(null);
-		return world;
+		when(clientWorld.getBlockState(any())).thenReturn(Blocks.AIR.getDefaultState());
+		when(serverWorld.getBlockState(any())).thenReturn(Blocks.AIR.getDefaultState());
+		when(clientWorld.getTileEntity(any())).thenReturn(null);
+		when(serverWorld.getTileEntity(any())).thenReturn(null);
 	}
 
 	/**
-	 * Puts the given block into the mock world at the given position.
+	 * Puts the given block into the mock worlds at the given position.
 	 *
 	 * @param world A mock world
 	 * @param x x-coordinate
@@ -72,12 +107,12 @@ public abstract class AbstractWorldBehaviorTest {
 	 * @param z z-coordinate
 	 * @param block The block to put
 	 */
-	protected void placeBlockAt(IBlockAccess world, int x, int y, int z, Block block) {
-		placeBlockAt(world, x, y, z, block.getDefaultState());
+	protected void placeBlockAt(int x, int y, int z, Block block) {
+		placeBlockAt(x, y, z, block.getDefaultState());
 	}
 
 	/**
-	 * Puts the given block into the mock world at the given position.
+	 * Puts the given block into the mock worlds at the given position.
 	 *
 	 * @param world A mock world
 	 * @param x x-coordinate
@@ -85,12 +120,16 @@ public abstract class AbstractWorldBehaviorTest {
 	 * @param z z-coordinate
 	 * @param block The block to put
 	 */
-	protected void placeBlockAt(IBlockAccess world, int x, int y, int z, IBlockState state) {
-		when(world.getBlockState(new BlockPos(x, y, z))).thenReturn(state);
+	protected void placeBlockAt(int x, int y, int z, IBlockState state) {
+		BlockPos pos = new BlockPos(x, y, z);
+
+		when(clientWorld.getBlockState(pos)).thenReturn(state);
+		when(serverWorld.getBlockState(pos)).thenReturn(state);
 	}
 
 	/**
-	 * Puts the given block entity into the mock world at the given position.
+	 * Puts the given block entity into the mock worlds at the given position.
+	 * The server world gets the exact block entity; the client gets a default one.
 	 *
 	 * @param world A mock world
 	 * @param x x-coordinate
@@ -98,7 +137,106 @@ public abstract class AbstractWorldBehaviorTest {
 	 * @param z z-coordinate
 	 * @param te The block entity to put
 	 */
-	protected void placeTEAt(IBlockAccess world, int x, int y, int z, TileEntity te) {
-		when(world.getTileEntity(new BlockPos(x, y, z))).thenReturn(te);
+	protected void placeTEAt(int x, int y, int z, TileEntity te) {
+		BlockPos pos = new BlockPos(x, y, z);
+
+		origTEPoses.add(pos);
+
+		IBlockState curState = clientWorld.getBlockState(pos);
+		BlockContainer curBlock = (BlockContainer)(curState.getBlock());
+		TileEntity defaultAtPos = curBlock.createNewTileEntity(clientWorld, curBlock.getMetaFromState(curState));
+
+		when(clientWorld.getTileEntity(pos)).thenReturn(defaultAtPos);
+		when(serverWorld.getTileEntity(pos)).thenReturn(te);
+		te.setWorld(serverWorld);
+	}
+
+	/**
+	 * Makes a container as the client would have.
+	 *
+	 * @param x x-coordinate of the block to open
+	 * @param y y-coordinate of the block to open
+	 * @param z z-coordinate of the block to open
+	 */
+	protected Container makeClientContainer(int x, int y, int z) {
+		BlockPos pos = new BlockPos(x, y, z);
+		// This is a bit of a mess, but inventories are a bit of a mess.
+		// First, prepare an IInventory as the server would see it:
+		TileEntity serverTE = serverWorld.getTileEntity(pos);
+		// Preconditions for this to make sense
+		assertNotNull(serverTE);
+		assertTrue(serverTE instanceof IInventory);
+
+		IInventory serverInv;
+		if (serverWorld.getBlockState(pos).getBlock() instanceof BlockChest) {
+			// Special-casing for large chests
+			BlockChest chest = (BlockChest) serverWorld.getBlockState(pos).getBlock();
+			serverInv = chest.getLockableContainer(serverWorld, pos);
+		} else {
+			serverInv = (IInventory) serverTE;
+		}
+		String guiID = ((IInteractionObject) serverTE).getGuiID();
+
+		// Now do stuff as the client would see it...
+		// NetHandlerPlayClient.handleOpenWindow(SPacketOpenWindow)
+		IInventory clientInv;
+		if (guiID.equals("minecraft:container")) {
+			// Ender chest -- don't know if we can actually get here
+			clientInv = new InventoryBasic(serverInv.getDisplayName(),
+					serverInv.getSizeInventory());
+		} else { // skip a few
+			clientInv = new ContainerLocalMenu(guiID, serverInv.getDisplayName(),
+					serverInv.getSizeInventory());
+		}
+		// Copy items and fields (this normally happens later, but whatever)
+		for (int i = 0; i < serverInv.getSizeInventory(); i++) {
+			clientInv.setInventorySlotContents(i, serverInv.getStackInSlot(i).copy());
+		}
+		for (int i = 0; i < serverInv.getFieldCount(); i++) {
+			clientInv.setField(i, serverInv.getField(i));
+		}
+
+		// EntityPlayerSP.displayGUIChest(IInventory) found in handleOpenWindow
+		// melded with the container code in each GUI
+		Container container;
+		if ("minecraft:chest".equals(guiID)) {
+			container = new ContainerChest(player.inventory, clientInv, player);
+		} else if ("minecraft:hopper".equals(guiID)) {
+			container = new ContainerHopper(player.inventory, clientInv, player);
+		} else if ("minecraft:furnace".equals(guiID)) {
+			container = new ContainerFurnace(player.inventory, clientInv);
+		} else if ("minecraft:brewing_stand".equals(guiID)) {
+			container = new ContainerBrewingStand(player.inventory, clientInv);
+		} else if ("minecraft:beacon".equals(guiID)) {
+			container = new ContainerBeacon(player.inventory, clientInv);
+		} else if ("minecraft:dispenser".equals(guiID) && "minecraft:dropper".equals(guiID)) {
+			container = new ContainerDispenser(player.inventory, clientInv);
+		// } else if ("minecraft:shulker_box".equals(guiID)) {
+		//     container = new ContainerShulkerBox(player.inventory, clientInv, player);
+		} else {
+			// Unknown -- i.e. minecraft:container
+			container = new ContainerChest(player.inventory, clientInv, player);
+		}
+
+		return container;
+	}
+
+	/**
+	 * Checks that the saved world matches the original.
+	 */
+	protected void checkWorld() {
+		assertThat("Must save all TEs", tileEntities.keySet(), is(origTEPoses));
+
+		for (BlockPos pos : origTEPoses) {
+			TileEntity server = serverWorld.getTileEntity(pos);
+			TileEntity saved = tileEntities.get(pos);
+
+			NBTTagCompound serverNBT = new NBTTagCompound();
+			server.writeToNBT(serverNBT);
+			NBTTagCompound savedNBT = new NBTTagCompound();
+			saved.writeToNBT(savedNBT);
+
+			assertThat(savedNBT, is(serverNBT));
+		}
 	}
 }
--- share/src/test/java/wdl/DoubleChestTest.java
+++ share/src/test/java/wdl/DoubleChestTest.java
@@ -14,41 +14,40 @@
  */
 package wdl;
 
-import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
-
-import java.util.function.BiConsumer;
 
 import org.junit.Test;
 
-import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.ContainerChest;
-import net.minecraft.inventory.InventoryBasic;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
 
 public class DoubleChestTest extends AbstractWorldBehaviorTest {
 
 	@Test
 	public void testSimpleDoubleChest() {
-		IBlockAccess world = makeMockWorld();
-		placeBlockAt(world, 0, 0, 0, Blocks.CHEST);
-		placeBlockAt(world, 0, 0, 1, Blocks.CHEST);
+		// TODO: Handle orientation of chests
+		// Not important in this version, but will matter later
+
+		makeMockWorld();
+
+		placeBlockAt(0, 0, 0, Blocks.CHEST);
+		placeBlockAt(0, 0, 1, Blocks.CHEST);
 		TileEntityChest te1 = new TileEntityChest();
+		te1.setInventorySlotContents(2, new ItemStack(Items.BEEF));
 		TileEntityChest te2 = new TileEntityChest();
-		placeTEAt(world, 0, 0, 0, te1);
-		placeTEAt(world, 0, 0, 1, te2);
+		te1.setInventorySlotContents(8, new ItemStack(Items.COOKED_BEEF));
+		placeTEAt(0, 0, 0, te1);
+		placeTEAt(0, 0, 1, te2);
 
-		@SuppressWarnings("unchecked")
-		BiConsumer<BlockPos, TileEntityChest> method = mock(BiConsumer.class);
+		ContainerChest container = (ContainerChest) makeClientContainer(0, 0, 0);
 
-		ContainerChest container = new ContainerChest(new InventoryPlayer(null), new InventoryBasic("Test", true, 53), null);
-		assertTrue(WDLEvents.saveDoubleChest(new BlockPos(0, 0, 0), container, world, method));
+		assertTrue(WDLEvents.saveDoubleChest(new BlockPos(0, 0, 0), container, clientWorld, tileEntities::put));
 
-		verify(method).accept(new BlockPos(0, 0, 0), te1);
-		verify(method).accept(new BlockPos(0, 0, 1), te2);
+		checkWorld();
 	}
 
 }
