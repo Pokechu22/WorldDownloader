a1fef0e0b67031f282235f90174f82f47535cccd
Pokechu22 <Pokechu022@gmail.com>
2018-02-25 11:05:38 -0800

    Stop re-using 1.10.2-litemod code in 1.10-litemod
    There's no reason to have that be the _only_ one that isn't duplicated, and if at some point I need to make a change to 1.10 only, that'll cause problems.

---
+++ 1.10-litemod/src/java6/java/com/uyjulian/LiteModWDL/mixin/MixinCrashReport.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of LiteModWDL.  LiteModWDL contains the liteloader-specific
+ * code for World Downloader: A mod to make backups of your multiplayer worlds.
+ * http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/2520465
+ *
+ * Copyright (c) 2014 nairol, cubic72
+ * Copyright (c) 2017 Pokechu22, julialy
+ *
+ * This project is licensed under the MMPLv2.  The full text of the MMPL can be
+ * found in LICENSE.md, or online at https://github.com/iopleke/MMPLv2/blob/master/LICENSE.md
+ * For information about this the MMPLv2, see http://stopmodreposts.org/
+ *
+ * Do not redistribute (in modified or unmodified form) without prior permission.
+ */
+package com.uyjulian.LiteModWDL.mixin;
+
+import net.minecraft.crash.CrashReport;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CrashReport.class)
+public abstract class MixinCrashReport {
+	@Inject(method="populateEnvironment", at=@At("RETURN"))
+	private void onCrashReportPopulateEnvironment(CallbackInfo ci) {
+		try {
+			wdl.WDLHooks.onCrashReportPopulateEnvironment((CrashReport)(Object)this);
+		} catch (Throwable t) {
+			try {
+				final Logger LOGGER = LogManager.getLogger();
+				LOGGER.fatal("World Downloader: Failed to add crash info", t);
+				((CrashReport)(Object)this).getCategory().addCrashSectionThrowable("World Downloader - Fatal error in crash handler (see log)", t);
+			} catch (Throwable t2) {
+				System.err.println("WDL: Double failure adding info to crash report!");
+				t.printStackTrace();
+				t2.printStackTrace();
+			}
+		}
+	}
+}
