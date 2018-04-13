a21fdf1d80bfea9d4b35cfa0ef4bec8f4771a53d
Pokechu22 <Pokechu022@gmail.com>
2018-04-12 18:38:21 -0700

    Generalize the entity tracker test
    This is also a WIP

--- share/src/test/java/wdl/EntityUtilsTest.java
+++ share/src/test/java/wdl/EntityUtilsTest.java
@@ -16,9 +16,15 @@ package wdl;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.IntFunction;
 
 import org.junit.Test;
 import org.mockito.AdditionalAnswers;
@@ -30,6 +36,8 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.profiler.Profiler;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.storage.ISaveHandler;
 import net.minecraft.world.storage.WorldInfo;
@@ -37,10 +45,17 @@ import net.minecraft.world.storage.WorldInfo;
 /**
  * An experimental test around the entity tracking code.  Not particularly complete.
  */
-public class EntityTrackerTest extends MaybeMixinTest {
+public class EntityUtilsTest extends MaybeMixinTest {
 
 	@Test
-	public void test() {
+	public void simpleTrackerTest() {
+		runTrackerTest(EntityPig::new, 80, 10,
+				(tick, entity) -> true,
+				(tick) -> new Vec3d(-150 + tick, tick, -150 + tick));
+	}
+
+	protected void runTrackerTest(Function<World, ? extends Entity> entitySupplier, int threshold,
+			int serverViewDistance, BiPredicate<Integer, Entity> keepEntity, IntFunction<Vec3d> posFunc) {
 		WorldServer world = mock(WorldServer.class, withSettings().defaultAnswer(RETURNS_MOCKS)
 				.useConstructor(mock(MinecraftServer.class, RETURNS_MOCKS), mock(ISaveHandler.class), mock(WorldInfo.class), 0, mock(Profiler.class)));
 
@@ -50,57 +65,51 @@ public class EntityTrackerTest extends MaybeMixinTest {
 		doAnswer(AdditionalAnswers.<Entity>answerVoid(trackedEntities::remove)).when(player).removeEntity(any());
 		when(player.getServerWorld().getPlayerChunkMap().isPlayerWatchingChunk(eq(player), anyInt(), anyInt())).thenReturn(true);
 
-		List<Entity> entities = new ArrayList<>();
-		class EntityTrackCollection {
-			public List<Entity> list = new ArrayList<>();
-			public void add(Entity e) {
-				if (list.contains(e)) {
-					throw new RuntimeException("Tried to add an entity that was already present: " + e);
-				}
-				list.add(e);
-			}
-			public void remove(Entity e) {
-				if (!list.contains(e)) {
-					throw new RuntimeException("Tried to remove an entity that was not already present: " + e);
-				}
-
-				int threshold = 80; // XXX can't even get the threshold because EntityUtils doesn't work in tests
-				int serverViewDistance = 10; // XXX hardcoded for now
-
-				if (!EntityUtils.isWithinSavingDistance(e, player, threshold, serverViewDistance)) {
-					throw new RuntimeException("Unexpected removal of " + e);
-				}
-				System.out.println("-" + e);
-				list.remove(e);
-			}
-		}
-		EntityTrackCollection col = new EntityTrackCollection();
+		List<Entity> entities = new ArrayList<>(); // all known entities; if killed they're removed from this list
+		List<Entity> tracked = new ArrayList<>(); // entities being tracked by the mock player
 		player.connection = mock(NetHandlerPlayServer.class);
-		doAnswer(AdditionalAnswers.answerVoid(col::add)).when(player).addEntity(any());
-		doAnswer(AdditionalAnswers.answerVoid(col::remove)).when(player).removeEntity(any());
+
+		doAnswer(AdditionalAnswers.<Entity>answerVoid((e) -> {
+			assertThat("Tried to track an entity that was already tracked", tracked, not(hasItem(e)));
+			tracked.add(e);
+		})).when(player).addEntity(any());
+		doAnswer(AdditionalAnswers.<Entity>answerVoid((e) -> {
+			assertThat("Tried to untrack an entity that was not tracked", tracked, hasItem(e));
+			tracked.remove(e);
+
+			boolean isPresent = (entities.contains(e));
+			assertEquals(EntityUtils.isWithinSavingDistance(e, player, threshold, serverViewDistance), isPresent);
+		})).when(player).removeEntity(any());
 
 		world.playerEntities.add(player);
 
 		EntityTracker tracker = new EntityTracker(world);
 		// Required because world doesn't set it up right for a mock, and mocking it
 		// would be making assumptions about how this is calculated
-		tracker.setViewDistance(10);
+		tracker.setViewDistance(serverViewDistance);
 
-		int i = 0;
+		int eid = 0;
 		for (int x = -100; x <= 100; x += 10) {
 			for (int z = -100; z <= 100; z += 10) {
-				EntityPig pig = new EntityPig(world);
-				entities.add(pig);
-				pig.setEntityId(i++);
-				pig.posX = x;
-				pig.posZ = z;
-				tracker.track(pig);
+				Entity e = entitySupplier.apply(world);
+				entities.add(e);
+				e.setEntityId(eid++);
+				e.posX = x;
+				e.posZ = z;
+				tracker.track(e);
 			}
 		}
 
 		for (int tick = 0; tick <= 300; tick++) {
-			//TODO also remove some entities by death
-			player.posX = player.posZ = -150 + tick;
+			Vec3d pos = posFunc.apply(tick);
+			player.posX = pos.x;
+			player.posY = pos.y;
+			player.posZ = pos.z;
+			for (Iterator<Entity> itr = entities.iterator(); itr.hasNext();) {
+				if (!keepEntity.test(tick, itr.next())) {
+					itr.remove();
+				}
+			}
 			tracker.tick();
 		}
 	}
