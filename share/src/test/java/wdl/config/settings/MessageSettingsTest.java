e1a7fcfff578650c532b2e7e6c44bc317be9af5f
Pokechu22 <Pokechu022@gmail.com>
2018-09-13 20:15:59 -0700

    Add a (meaningless) test for MessageSettings

---
+++ share/src/test/java/wdl/config/settings/MessageSettingsTest.java
@@ -0,0 +1,32 @@
+/*
+ * This file is part of World Downloader: A mod to make backups of your
+ * multiplayer worlds.
+ * http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/2520465
+ *
+ * Copyright (c) 2014 nairol, cubic72
+ * Copyright (c) 2018 Pokechu22, julialy
+ *
+ * This project is licensed under the MMPLv2.  The full text of the MMPL can be
+ * found in LICENSE.md, or online at https://github.com/iopleke/MMPLv2/blob/master/LICENSE.md
+ * For information about this the MMPLv2, see http://stopmodreposts.org/
+ *
+ * Do not redistribute (in modified or unmodified form) without prior permission.
+ */
+package wdl.config.settings;
+
+import static wdl.config.settings.TestUtils.*;
+
+import org.junit.Test;
+
+import wdl.MaybeMixinTest;
+
+public class MessageSettingsTest extends MaybeMixinTest {
+
+	@Test
+	public void testEnableAll() {
+		checkAllText(MessageSettings.ENABLE_ALL_MESSAGES);
+	}
+
+	// MessageCategorySetting and MessageTypeSetting will be handled by the WDLMessages test
+
+}
