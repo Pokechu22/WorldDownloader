bcb74805b52950393b139145922775beffb6e8b2
Pokechu22 <Pokechu022@gmail.com>
2019-12-29 16:07:46 -0800

    Add code for 1.14.4 forge
    It still cannot be installed easily, and I haven't done too much
    testing, so I won't include it in the next build.  But it does at least
    appear to work.

---
+++ 1.14.4-forge/src/main/java/wdl/forge/WDLForgeMod.java
@@ -0,0 +1,27 @@
+/*
+ * This file is part of WDL Forge.  WDL Forge contains the forge-specific
+ * code for World Downloader: A mod to make backups of your multiplayer worlds.
+ * http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/2520465
+ *
+ * Copyright (c) 2014 nairol, cubic72
+ * Copyright (c) 2019 Pokechu22, julialy
+ *
+ * This project is licensed under the MMPLv2.  The full text of the MMPL can be
+ * found in LICENSE.md, or online at https://github.com/iopleke/MMPLv2/blob/master/LICENSE.md
+ * For information about this the MMPLv2, see http://stopmodreposts.org/
+ *
+ * Do not redistribute (in modified or unmodified form) without prior permission.
+ */
+package wdl.forge;
+
+import net.minecraftforge.fml.common.Mod;
+import wdl.api.APIImpl;
+
+@Mod("wdl")
+public class WDLForgeMod {
+	public WDLForgeMod() {
+		// Since Forge generates a crash report on startup, which will also include
+		// WDL crash info, we want to make sure the API is initialized.
+		APIImpl.ensureInitialized();
+	}
+}
