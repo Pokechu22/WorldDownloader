58be955cc6b08570891de5270c43f71e1671ad4a
Pokechu22 <Pokechu022@gmail.com>
2020-01-21 21:15:06 -0800

    Revert "Resolve the other uses of GlStateManager"
    This reverts commit b5e7f00ea12d311852a5161f5754c83e0bc621a1.

--- share_14/src/main/java/wdl/gui/widget/GuiSlider.java
+++ share_14/src/main/java/wdl/gui/widget/GuiSlider.java
@@ -13,7 +13,7 @@
  */
 package wdl.gui.widget;
 
-//import com.mojang.blaze3d.platform.GlStateManager; XXX
+import com.mojang.blaze3d.platform.GlStateManager;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
@@ -94,7 +94,7 @@ public class GuiSlider extends ExtButton {
 	@Override
 	public void midDraw() {
 		Minecraft.getInstance().getTextureManager().bindTexture(WIDGETS_LOCATION);
-		//GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F); XXX reenable when names match in 1.15
+		GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
 
 		if (this.isEnabled()) {
 			this.blit(this.x
