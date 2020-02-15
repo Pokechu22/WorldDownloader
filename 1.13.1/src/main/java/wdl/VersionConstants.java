222cd11864302e4be50a448ea671dbfa2dd3d790
Pokechu22 <Pokechu022@gmail.com>
2020-02-15 12:35:59 -0800

    Version 4.0.6.1

--- 1.13.1/src/main/java/wdl/VersionConstants.java
+++ 1.13.1/src/main/java/wdl/VersionConstants.java
@@ -85,7 +85,7 @@ public class VersionConstants {
 	 */
 	public static String getModVersion() {
 		// TODO: Automatically generate this somehow
-		return "4.0.6.1-SNAPSHOT";
+		return "4.0.6.1";
 	}
 
 	/**
