008e8ac96cb0f867dc7e0b87d982734497b3e118
Pokechu22 <Pokechu022@gmail.com>
2020-08-04 21:47:22 -0700

    Create version-specific WDLChunkLoader, DelegateChunkManager, & ExtWorld
    These haven't been changed from the previous versions yet.

---
+++ share_14/src/main/java/wdl/WDLChunkLoader_1_16.java
@@ -0,0 +1,291 @@
+/*
+ * This file is part of World Downloader: A mod to make backups of your multiplayer worlds.
+ * https://www.minecraftforum.net/forums/mapping-and-modding-java-edition/minecraft-mods/2520465-world-downloader-mod-create-backups-of-your-builds
+ *
+ * Copyright (c) 2014 nairol, cubic72
+ * Copyright (c) 2018-2019 Pokechu22, julialy
+ *
+ * This project is licensed under the MMPLv2.  The full text of the MMPL can be
+ * found in LICENSE.md, or online at https://github.com/iopleke/MMPLv2/blob/master/LICENSE.md
+ * For information about this the MMPLv2, see https://stopmodreposts.org/
+ *
+ * Do not redistribute (in modified or unmodified form) without prior permission.
+ */
+package wdl;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map.Entry;
+
+import it.unimi.dsi.fastutil.longs.Long2ObjectLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.shorts.ShortList;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.nbt.LongArrayNBT;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.SectionPos;
+import net.minecraft.util.palette.UpgradeData;
+import net.minecraft.world.LightType;
+import net.minecraft.world.SerializableTickList;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeContainer;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkPrimerTickList;
+import net.minecraft.world.chunk.ChunkSection;
+import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.chunk.IChunk;
+import net.minecraft.world.chunk.NibbleArray;
+import net.minecraft.world.chunk.storage.ChunkLoader;
+import net.minecraft.world.chunk.storage.IOWorker;
+import net.minecraft.world.chunk.storage.RegionFile;
+import net.minecraft.world.chunk.storage.RegionFileCache;
+import net.minecraft.world.dimension.Dimension;
+import net.minecraft.world.dimension.EndDimension;
+import net.minecraft.world.dimension.NetherDimension;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.lighting.WorldLightManager;
+import net.minecraft.world.server.ServerTickList;
+import net.minecraft.world.storage.SaveHandler;
+import net.minecraft.world.storage.SessionLockException;
+import wdl.config.settings.MiscSettings;
+import wdl.versioned.VersionedFunctions;
+
+/**
+ * Alternative implementation of {@link ChunkLoader} that handles editing
+ * WDL-specific properties of chunks as they are being saved.
+ *
+ * This variant is used for chunks from 1.13 and later.
+ */
+abstract class WDLChunkLoaderBase extends ChunkLoader {
+
+	/**
+	 * Gets the save folder for the given WorldProvider, respecting Forge's
+	 * dimension names if forge is present.
+	 */
+	protected static File getWorldSaveFolder(SaveHandler handler,
+			Dimension dimension) {
+		File baseFolder = handler.getWorldDirectory();
+		// XXX No forge support at this time
+
+		File dimensionFolder;
+		if (WDL.serverProps.getValue(MiscSettings.FORCE_DIMENSION_TO_OVERWORLD)) {
+			dimensionFolder = baseFolder;
+		} else if (dimension instanceof NetherDimension) {
+			dimensionFolder = new File(baseFolder, "DIM-1");
+		} else if (dimension instanceof EndDimension) {
+			dimensionFolder = new File(baseFolder, "DIM1");
+		} else {
+			// Assume that this is the overworld.
+			dimensionFolder = baseFolder;
+		}
+
+		return new File(dimensionFolder, "region");
+	}
+
+	protected final WDL wdl;
+	/**
+	 * Location where chunks are saved.
+	 *
+	 * In this version, this directly is the region folder for the given dimension;
+	 * for the overworld it is world/region and others it is world/DIM#/region.
+	 */
+	protected final File chunkSaveLocation;
+
+	// XXX HACK this is burried deep, and probably shouldn't be directly accessed
+	protected final Long2ObjectLinkedOpenHashMap<RegionFile> cache;
+
+	@SuppressWarnings({ "resource", "unchecked" })
+	protected WDLChunkLoaderBase(WDL wdl, File file) {
+		super(file, null);
+		this.wdl = wdl;
+		this.chunkSaveLocation = file;
+		IOWorker worker = ReflectionUtils.findAndGetPrivateField(this, ChunkLoader.class, IOWorker.class);
+		RegionFileCache rfc = ReflectionUtils.findAndGetPrivateField(worker, RegionFileCache.class);
+		this.cache = ReflectionUtils.findAndGetPrivateField(rfc, Long2ObjectLinkedOpenHashMap.class);
+	}
+
+	/**
+	 * Saves the given chunk.
+	 *
+	 * Note that while the normal implementation swallows Exceptions, this
+	 * version does not.
+	 */
+	public synchronized void saveChunk(World world, IChunk chunk) throws SessionLockException, IOException {
+		wdl.saveHandler.checkSessionLock();
+
+		CompoundNBT levelTag = writeChunkToNBT((Chunk)chunk, world);
+
+		CompoundNBT rootTag = new CompoundNBT();
+		rootTag.put("Level", levelTag);
+		rootTag.putInt("DataVersion", VersionConstants.getDataVersion());
+
+		writeChunk(chunk.getPos(), rootTag);
+
+		wdl.unloadChunk(chunk.getPos());
+	}
+
+	/**
+	 * Writes the given chunk, creating an NBT compound tag.
+	 *
+	 * Note that this does <b>not</b> override the private method
+	 * {@link AnvilChunkLoader#writeChunkToNBT(Chunk, World, NBTCompoundNBT)}.
+	 * That method is private and cannot be overridden; plus, this version
+	 * returns a tag rather than modifying the one passed as an argument.
+	 *
+	 * @param chunk
+	 *            The chunk to write
+	 * @param world
+	 *            The world the chunk is in, used to determine the modified
+	 *            time.
+	 * @return A new CompoundNBT
+	 */
+	private CompoundNBT writeChunkToNBT(Chunk chunk, World world) {
+		CompoundNBT compound = new CompoundNBT();
+
+		ChunkPos chunkpos = chunk.getPos();
+		compound.putInt("xPos", chunkpos.x);
+		compound.putInt("zPos", chunkpos.z);
+		compound.putLong("LastUpdate", world.getGameTime());
+		compound.putLong("InhabitedTime", chunk.getInhabitedTime());
+		compound.putString("Status", ChunkStatus.FULL.getName()); // Make sure that the chunk is considered fully generated
+		UpgradeData upgradedata = chunk.getUpgradeData();
+
+		if (!upgradedata.isEmpty()) {
+			compound.put("UpgradeData", upgradedata.write());
+		}
+
+		ChunkSection[] chunkSections = chunk.getSections();
+		ListNBT chunkSectionList = new ListNBT();
+		WorldLightManager worldlightmanager = world.getChunkProvider().getLightManager();
+
+		// XXX: VersionedFunctions.hasSkyLight is inapplicable here presumably, but it might still need to be used somehow
+		for (int y = -1; y < 17; ++y) {
+			final int f_y = y; // Compiler food
+			ChunkSection chunkSection = Arrays.stream(chunkSections)
+					.filter(section -> section != null && section.getYLocation() >> 4 == f_y)
+					.findFirst()
+					.orElse(Chunk.EMPTY_SECTION);
+			NibbleArray blocklightArray = worldlightmanager.getLightEngine(LightType.BLOCK)
+					.getData(SectionPos.from(chunkpos, y));
+			NibbleArray skylightArray = worldlightmanager.getLightEngine(LightType.SKY)
+					.getData(SectionPos.from(chunkpos, y));
+			if (chunkSection != Chunk.EMPTY_SECTION || blocklightArray != null || skylightArray != null) {
+				CompoundNBT sectionNBT = new CompoundNBT();
+				sectionNBT.putByte("Y", (byte) (y & 255));
+				if (chunkSection != Chunk.EMPTY_SECTION) {
+					chunkSection.getData().writeChunkPalette(sectionNBT, "Palette", "BlockStates");
+				}
+
+				if (blocklightArray != null && !blocklightArray.isEmpty()) {
+					sectionNBT.putByteArray("BlockLight", blocklightArray.getData());
+				}
+
+				if (skylightArray != null && !skylightArray.isEmpty()) {
+					sectionNBT.putByteArray("SkyLight", skylightArray.getData());
+				}
+
+				chunkSectionList.add(sectionNBT);
+			}
+		}
+
+		compound.put("Sections", chunkSectionList);
+
+		if (chunk.hasLight()) {
+			compound.putBoolean("isLightOn", true);
+		}
+
+		BiomeContainer biomes = chunk.getBiomes();
+		if (biomes != null) {
+			compound.putIntArray("Biomes", biomes.getBiomeIds());
+		}
+
+		chunk.setHasEntities(false);
+		ListNBT entityList = getEntityList(chunk);
+		compound.put("Entities", entityList);
+
+		ListNBT tileEntityList = getTileEntityList(chunk);
+		compound.put("TileEntities", tileEntityList);
+
+		// XXX: Note: This was re-sorted on mojang's end; I've undone that.
+		if (world.getPendingBlockTicks() instanceof ServerTickList) {
+			compound.put("TileTicks", ((ServerTickList<?>) world.getPendingBlockTicks()).func_219503_a(chunkpos));
+		}
+		if (world.getPendingFluidTicks() instanceof ServerTickList) {
+			compound.put("LiquidTicks", ((ServerTickList<?>) world.getPendingFluidTicks()).func_219503_a(chunkpos));
+		}
+
+		compound.put("PostProcessing", listArrayToTag(chunk.getPackedPositions()));
+
+		if (chunk.getBlocksToBeTicked() instanceof ChunkPrimerTickList) {
+			compound.put("ToBeTicked", ((ChunkPrimerTickList<?>) chunk.getBlocksToBeTicked()).write());
+		}
+
+		// XXX: These are new, and they might conflict with the other one.  Not sure which should be used.
+		if (chunk.getBlocksToBeTicked() instanceof SerializableTickList) {
+			compound.put("TileTicks", ((SerializableTickList<?>) chunk.getBlocksToBeTicked())
+					.save(world.getGameTime()));
+		}
+
+		if (chunk.getFluidsToBeTicked() instanceof ChunkPrimerTickList) {
+			compound.put("LiquidsToBeTicked", ((ChunkPrimerTickList<?>) chunk.getFluidsToBeTicked()).write());
+		}
+
+		if (chunk.getFluidsToBeTicked() instanceof SerializableTickList) {
+			compound.put("LiquidTicks", ((SerializableTickList<?>) chunk.getFluidsToBeTicked())
+					.save(world.getGameTime()));
+		}
+
+		CompoundNBT heightMaps = new CompoundNBT();
+
+		for (Entry<Heightmap.Type, Heightmap> entry : chunk.getHeightmaps()) {
+			if (chunk.getStatus().getHeightMaps().contains(entry.getKey())) {
+				heightMaps.put(entry.getKey().getId(), new LongArrayNBT(entry.getValue().getDataArray()));
+			}
+		}
+
+		compound.put("Heightmaps", heightMaps);
+		// TODO
+		//compound.put("Structures",
+		//		writeStructures(chunkpos, chunk.getStructureStarts(), chunk.getStructureReferences()));
+		return compound;
+	}
+
+	protected abstract ListNBT getEntityList(Chunk chunk);
+	protected abstract ListNBT getTileEntityList(Chunk chunk);
+
+	/**
+	 * Gets a count of how many chunks there are that still need to be written to
+	 * disk. (Does not include any chunk that is currently being written to disk)
+	 *
+	 * @return The number of chunks that still need to be written to disk
+	 */
+	public synchronized int getNumPendingChunks() {
+		return this.cache.size(); // XXX This is actually the number of regions
+	}
+
+	private ListNBT listArrayToTag(ShortList[] list) {
+		ListNBT listnbt = new ListNBT();
+
+		for (ShortList shortlist : list) {
+			ListNBT sublist;
+			if (shortlist != null) {
+				sublist = VersionedFunctions.createShortListTag(shortlist.toShortArray());
+			} else {
+				sublist = VersionedFunctions.createShortListTag();
+			}
+
+			listnbt.add(sublist);
+		}
+
+		return listnbt;
+	}
+
+	/**
+	 * Provided since the constructor changes between versions.
+	 */
+	protected RegionFile createRegionFile(File file) throws IOException {
+		return new RegionFile(file, this.chunkSaveLocation);
+	}
+}
---
+++ share_14/src/test/java/wdl/ExtWorld_1_16.java
@@ -0,0 +1,305 @@
+/*
+ * This file is part of World Downloader: A mod to make backups of your multiplayer worlds.
+ * https://www.minecraftforum.net/forums/mapping-and-modding-java-edition/minecraft-mods/2520465-world-downloader-mod-create-backups-of-your-builds
+ *
+ * Copyright (c) 2014 nairol, cubic72
+ * Copyright (c) 2018-2020 Pokechu22, julialy
+ *
+ * This project is licensed under the MMPLv2.  The full text of the MMPL can be
+ * found in LICENSE.md, or online at https://github.com/iopleke/MMPLv2/blob/master/LICENSE.md
+ * For information about this the MMPLv2, see https://stopmodreposts.org/
+ *
+ * Do not redistribute (in modified or unmodified form) without prior permission.
+ */
+package wdl;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.Arrays;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockEventData;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.multiplayer.ClientChunkProvider;
+import net.minecraft.client.network.play.ClientPlayNetHandler;
+import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.item.crafting.RecipeManager;
+import net.minecraft.profiler.IProfiler;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.ServerScoreboard;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldSettings;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeContainer;
+import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.chunk.AbstractChunkProvider;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.chunk.IChunk;
+import net.minecraft.world.server.ServerChunkProvider;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.storage.SaveHandler;
+import net.minecraft.world.storage.WorldInfo;
+
+abstract class ExtWorldClient extends ClientWorld {
+	private static final int VIEW_DISTANCE = 16;
+
+	public ExtWorldClient(ClientPlayNetHandler netHandler, WorldSettings settings, DimensionType dim,
+			Difficulty difficulty, IProfiler profilerIn) {
+		super(netHandler, settings, dim, VIEW_DISTANCE, profilerIn, mock(WorldRenderer.class));
+		getChunkProvider(); // Make sure it's injected
+	}
+
+	private boolean injectedChunkProvider = false;
+
+	@SuppressWarnings("resource")
+	@Override
+	public ClientChunkProvider getChunkProvider() {
+		if (!injectedChunkProvider) {
+			// Also, note that this is changing a final field, but doesn't seem to be causing any issues...
+			ReflectionUtils.findAndSetPrivateField(this, World.class, AbstractChunkProvider.class, new SimpleChunkProvider());
+			assertThat(this.chunkProvider, is(instanceOf(SimpleChunkProvider.class)));
+			injectedChunkProvider = true;
+		}
+		return super.getChunkProvider();
+	}
+
+	private final RecipeManager recipeManager = mock(RecipeManager.class);
+	private final Scoreboard scoreboard = mock(Scoreboard.class, withSettings().useConstructor());
+
+	@Override
+	public RecipeManager getRecipeManager() {
+		return this.recipeManager;
+	}
+
+	@Override
+	public Scoreboard getScoreboard() {
+		return this.scoreboard;
+	}
+
+	/** Places a block, creating the proper state. */
+	public void placeBlockAt(BlockPos pos, Block block, PlayerEntity player, Direction direction) {
+		player.rotationYaw = direction.getHorizontalAngle();
+		player.setHeldItem(Hand.MAIN_HAND, new ItemStack(block));
+		BlockItemUseContext context = new BlockItemUseContext(
+				new ItemUseContext(player, Hand.MAIN_HAND,
+						new BlockRayTraceResult(new Vector3d(pos), direction, pos, false)));
+		BlockState state = block.getStateForPlacement(context);
+		setBlockState(pos, state);
+	}
+
+	/** Defers to the 1.13.1-specific tick method */
+	public void tick() {
+		this.tick(() -> true);
+	}
+
+	@Override
+	public void playSound(double p_184134_1_, double p_184134_3_, double p_184134_5_, SoundEvent p_184134_7_,
+			SoundCategory p_184134_8_, float p_184134_9_, float p_184134_10_, boolean p_184134_11_) {
+		// Do nothing
+	}
+
+	private static final BiomeContainer NO_BIOMES;
+	static {
+		Biome[] biomes = new Biome[BiomeContainer.BIOMES_SIZE];
+		Arrays.fill(biomes, Biomes.THE_VOID);
+		NO_BIOMES = new BiomeContainer(biomes);
+	}
+
+	/**
+	 * A simple chunk provider implementation that just caches chunks in a map.
+	 */
+	private final class SimpleChunkProvider extends ClientChunkProvider {
+		private final Long2ObjectMap<Chunk> map = new Long2ObjectOpenHashMap<>();
+
+		public SimpleChunkProvider() {
+			super(ExtWorldClient.this, VIEW_DISTANCE);
+		}
+
+		@Override
+		public Chunk getChunk(int x, int z, ChunkStatus requiredStatus, boolean load) {
+			return map.computeIfAbsent(ChunkPos.asLong(x, z), k -> new Chunk(ExtWorldClient.this, new ChunkPos(x, z), NO_BIOMES));
+		}
+
+		@Override
+		public void tick(BooleanSupplier hasTimeLeft) {
+		}
+
+		@Override
+		public String makeString() {
+			return this.toString();
+		}
+
+		@Override
+		public boolean chunkExists(int x, int z) {
+			return true;
+		}
+	}
+}
+
+abstract class ExtWorldServer extends ServerWorld {
+	public ExtWorldServer(MinecraftServer server, SaveHandler saveHandlerIn, WorldInfo info, DimensionType dim,
+			IProfiler profilerIn) {
+		super(server, task -> task.run(), saveHandlerIn, info, dim, profilerIn, null);
+		getChunkProvider(); // Make sure it's injected
+	}
+
+	/**
+	 * Class of the real/vanilla ServerWorld.
+	 */
+	public static final Class<? super ExtWorldServer> SERVER_WORLD_CLASS = ServerWorld.class;
+
+	private final RecipeManager recipeManager = mock(RecipeManager.class);
+	private final ServerScoreboard scoreboard = mock(ServerScoreboard.class, withSettings().useConstructor(new Object[] {null}));
+	private boolean injectedChunkProvider = false;
+
+	@SuppressWarnings("resource")
+	@Override
+	public ServerChunkProvider getChunkProvider() {
+		if (!injectedChunkProvider) {
+			// NOTE: we have to do this here instead of the constructor, since it's called in the parent's constructor
+			// Also, note that this is changing a final field, but doesn't seem to be causing any issues...
+			ReflectionUtils.findAndSetPrivateField(this, World.class, AbstractChunkProvider.class, new SimpleChunkProvider());
+			assertThat(this.chunkProvider, is(instanceOf(SimpleChunkProvider.class)));
+			injectedChunkProvider = true;
+		}
+		return super.getChunkProvider();
+	}
+
+	@Override
+	public RecipeManager getRecipeManager() {
+		return this.recipeManager;
+	}
+
+	@Override
+	public ServerScoreboard getScoreboard() {
+		return this.scoreboard;
+	}
+
+	/** Places a block, creating the proper state. */
+	public void placeBlockAt(BlockPos pos, Block block, PlayerEntity player, Direction direction) {
+		player.rotationYaw = direction.getHorizontalAngle();
+		BlockItemUseContext context = new BlockItemUseContext(
+				new ItemUseContext(player, Hand.MAIN_HAND,
+						new BlockRayTraceResult(new Vector3d(pos), direction, pos, false)));
+		BlockState state = block.getStateForPlacement(context);
+		setBlockState(pos, state);
+	}
+
+	/** Left-clicks a block. */
+	public void clickBlock(BlockPos pos, PlayerEntity player) {
+		BlockState state = this.getBlockState(pos);
+		state.onBlockClicked(this, pos, player);
+	}
+
+	/** Right-clicks a block. */
+	public void interactBlock(BlockPos pos, PlayerEntity player) {
+		BlockState state = this.getBlockState(pos);
+		BlockRayTraceResult rayTraceResult = new BlockRayTraceResult(new Vector3d(pos), Direction.DOWN, pos, false);
+		state.onBlockActivated(this, player, Hand.MAIN_HAND, rayTraceResult);
+	}
+
+	/** Right-clicks an entity. */
+	public void interactEntity(Entity entity, PlayerEntity player) {
+		entity.processInitialInteract(player, Hand.MAIN_HAND);
+	}
+
+	public Consumer<BlockEventData> onBlockEvent = (e -> super.addBlockEvent(e.getPosition(), e.getBlock(), e.getEventID(), e.getEventParameter()));
+
+	@Override
+	public void addBlockEvent(BlockPos pos, Block blockIn, int eventID, int eventParam) {
+		onBlockEvent.accept(new BlockEventData(pos, blockIn, eventID, eventParam));
+	}
+
+	/** Defers to the 1.13.1-specific tick method */
+	public void tick() {
+		this.tick(() -> true);
+	}
+
+	@Override
+	public void createSpawnPosition(WorldSettings p_73052_1_) {
+		// Do nothing
+	}
+
+	@Override
+	public void playSound(double p_184134_1_, double p_184134_3_, double p_184134_5_, SoundEvent p_184134_7_,
+			SoundCategory p_184134_8_, float p_184134_9_, float p_184134_10_, boolean p_184134_11_) {
+		// Do nothing
+	}
+
+	private static final BiomeContainer NO_BIOMES;
+	static {
+		Biome[] biomes = new Biome[BiomeContainer.BIOMES_SIZE];
+		Arrays.fill(biomes, Biomes.THE_VOID);
+		NO_BIOMES = new BiomeContainer(biomes);
+	}
+
+	/**
+	 * A simple chunk provider implementation that just caches chunks in a map.
+	 */
+	private final class SimpleChunkProvider extends ExtServerChunkProvider {
+		private final Long2ObjectMap<Chunk> map = new Long2ObjectOpenHashMap<>();
+
+		public SimpleChunkProvider() {
+			super(ExtWorldServer.this, null, null, null, task -> task.run(), null, 0, null, null);
+		}
+
+		@Override
+		public IChunk getChunk(int x, int z, ChunkStatus p_212849_3_, boolean p_212849_4_) {
+			return map.computeIfAbsent(ChunkPos.asLong(x, z), k -> new Chunk(ExtWorldServer.this, new ChunkPos(x, z), NO_BIOMES));
+		}
+
+		@Override
+		public void tick(BooleanSupplier hasTimeLeft) {
+		}
+
+		@Override
+		public String makeString() {
+			return this.toString();
+		}
+
+		@Override
+		public int getLoadedChunkCount() {
+			return map.size();
+		}
+
+		@Override
+		public boolean chunkExists(int x, int z) {
+			return true;
+		}
+
+		@Override
+		public ChunkGenerator<?> getChunkGenerator() {
+			return null; // This is allowed, albeit awkward; see ChunkProviderClient
+		}
+	}
+
+	public void setPlayerSneaking(PlayerEntity player, boolean sneaking) {
+		// Exists only due to name issues
+		player.setSneaking(sneaking);
+	}
+}
